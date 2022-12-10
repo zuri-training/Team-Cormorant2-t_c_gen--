d30598a2a5ad877cdc188d41e2bc4078f844fbcc
unknown <shonowosolomon@yahoo.com>
2022-12-10 12:43:00 +0100

    adding privacy html

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,11 @@
 const express = require('express');
+
 const app = express();
-const cors = require('cors');
-// DOTENV enables our server reads our secret codes existing in oour .env file
-require("dotenv").config();
 
-// Database
-const connectDatabase = require("./config/dataBase");
+const PORT = process.env.PORT || 3000;
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
+app.get('/', (req,res) => {
+    res.send('zuri poject')
+});
 
-// Router
-const authenticationRouter = require("./router/authRoutes")
-
-
-app.use("/api/auth", authenticationRouter)
-
-const port = process.env.PORT || 5000
-
-const startServer = () => {
-    try {
-        connectDatabase(process.env.MONGO_URI);
-        app.listen(port, () => console.log(`Server is listening on port ${port}`))
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-startServer();
+app.listen(PORT,() => console.log("serving on port ${port}"));
